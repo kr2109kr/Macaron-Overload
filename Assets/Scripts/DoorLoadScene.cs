d95fc4949941f271ba441e9aba6f3f1a480b3542
kr2109kr <kr2109.kr@gmail.com>
2025-01-30 02:28:26 +0700

    Door: Add Load-Scene

---
+++ Assets/Scripts/DoorLoadScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorLoadScene : MonoBehaviour
+{
+    [SerializeField] Object scene;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
